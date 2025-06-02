e9a8805b8e912ecbeb479c160de52a748d898947
Omar Valerio <omar.valerio@gmail.com>
2025-06-02 18:09:21 +0200

    DeepLabCut 3.x pre-release is the one with pytorch support. (installing from PyPI)

--- conda/install_cpu.sh
+++ conda/install_cpu.sh
@@ -3,8 +3,9 @@ source ~/anaconda3/bin/activate
 conda env create -f amadeusGPT.yml
 conda activate amadeusgpt
 conda install pytorch torchvision cpuonly -c pytorch
-pip install "git+https://github.com/DeepLabCut/DeepLabCut.git@pytorch_dlc#egg=deeplabcut"
-pip install pycocotools
-pip install -e .[streamlit]
+# Install the latest version of DeepLabCut with PyTorch support
+python -m pip install --pre deeplabcut
+python -m pip install pycocotools
+python -m pip install -e .[streamlit]
 # install the python kernel
 python -m ipykernel install --user --name amadeusgpt --display-name "amadeusgpt"
--- conda/install_gpu.sh
+++ conda/install_gpu.sh
@@ -4,8 +4,9 @@ conda env create -f amadeusGPT.yml
 conda activate amadeusgpt
 # adjust this line according to your cuda version
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
-pip install "git+https://github.com/DeepLabCut/DeepLabCut.git@pytorch_dlc#egg=deeplabcut"
-pip install pycocotools
-pip install -e .[streamlit]
+# Install the latest version of DeepLabCut with PyTorch support
+python -m pip install --pre deeplabcut
+python -m pip install pycocotools
+python -m pip install -e .[streamlit]
 # install the python kernel
 python -m ipykernel install --user --name amadeusgpt --display-name "amadeusgpt"
